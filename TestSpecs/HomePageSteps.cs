b6b0835200296bc4423a8b0563992657bc3dc07e
Brad Cunningham <Brad@CraftingBytes.com>
2016-09-06 15:01:03 -0700

    spaces to tabs

--- TestSpecs/HomePageSteps.cs
+++ TestSpecs/HomePageSteps.cs
@@ -1,24 +1,25 @@
 ﻿using System;
+using BasePageObjectModel;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 using TestPageObjectModel;
 
 namespace TestSpecs
 {
-    [Binding]
-    public class HomePageSteps
-    {
-        [Given(@"I am on the home page")]
-        public void GivenIAmOnTheHomePage()
-        {
-	        Pages.Home.GoTo();
-        }
-        
-        [Then(@"the title should be showing")]
-        public void ThenTheTitleShouldBeShowing()
-        {
-	        var title = Pages.Home.GetTitle();
+	[Binding]
+	public class HomePageSteps
+	{
+		[Given(@"I am on the home page")]
+		public void GivenIAmOnTheHomePage()
+		{
+			Pages.Home.GoTo();
+		}
+
+		[Then(@"the title should be showing")]
+		public void ThenTheTitleShouldBeShowing()
+		{
+			var title = Pages.Home.GetTitle();
 			Assert.IsNotNull(title);
-        }
-    }
+		}
+	}
 }
