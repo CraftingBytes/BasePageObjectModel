3df88685457b75e14ff4994f115ca3fb70fc7b08
Brad Cunningham <Brad@CraftingBytes.com>
2016-09-14 15:22:37 -0700

    Increase timeouts

--- BasePageObjectModel/WaitExtensions.cs
+++ BasePageObjectModel/WaitExtensions.cs
@@ -1,32 +1,36 @@
-﻿using OpenQA.Selenium;
-using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading;
+﻿using System;
 using OpenQA.Selenium.Support.UI;
 
 namespace BasePageObjectModel
 {
 	public static class WaitExtensions
 	{
-		public static bool ClickAndWaitForLoad<T>(this T page, Action<T> action) where T : BasePage
+		public static bool ClickAndWaitForLoad<T>(this T page, Action<T> action, TimeSpan? timeout = null) where T : BasePage
 		{
 			var urlBeforeClick = page.WebDriver.Url;
 			action(page);
-			return page.WaitForLoad(urlBeforeClick);
+			return page.WaitForLoad(urlBeforeClick,timeout);
 		}
 
-		public static bool WaitForLoad(this BasePage basePage, string urlBeforeClick)
+		public static bool WaitForLoad(this BasePage basePage, string urlBeforeClick, TimeSpan? timeout = null)
 		{
-			return new WebDriverWait(basePage.WebDriver, TimeSpan.FromSeconds(2)).Until(wd => wd.Url != urlBeforeClick);
+			if (timeout == null)
+			{
+				timeout = TimeSpan.FromSeconds(10);
+			}
+			return new WebDriverWait(basePage.WebDriver, timeout.Value).Until(wd => wd.Url != urlBeforeClick);
 		}
 
-		public static bool PerformActionOnElementAndWaitForReload<T>(this T page, Action<T> action) where T : BaseElementContainer
+		public static bool PerformActionOnElementAndWaitForReload<T>(this T page, Action<T> action, TimeSpan? timeout = null) where T : BaseElementContainer
 		{
+			if (timeout == null)
+			{
+				timeout = TimeSpan.FromSeconds(10);
+			}
+
 			var oldPageSource = page.WebDriver.PageSource;
 			action(page);
-			return new WebDriverWait(page.WebDriver, TimeSpan.FromSeconds(2)).Until(wd => wd.PageSource != oldPageSource);
+			return new WebDriverWait(page.WebDriver, timeout.Value).Until(wd => wd.PageSource != oldPageSource);
 		}
 	}
 }
